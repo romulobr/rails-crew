3c5f7f069cb720c801690eeaf05af6e262b07e91
Rom <romulo.santos@travelperk.com>
2022-07-18 03:34:46 +0200

    sets up client app with apollo client

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,26 +1,46 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {ApolloClient, InMemoryCache, ApolloProvider, gql} from '@apollo/client';
+
+const client = new ApolloClient({
+    uri: 'http://localhost:3000/graphql',
+    cache: new InMemoryCache(),
+});
+
+client
+.query({
+    query: gql`
+        query GetPlayers {
+            players {
+                id
+                name
+            }
+        }
+    `,
+})
+.then((result) => console.log(result));
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <p>
+                    Edit <code>src/App.tsx</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+        </div>
+    );
 }
 
 export default App;
