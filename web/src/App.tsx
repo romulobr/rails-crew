854682cc963cee8b0b5f7d78deb07d3cda11bebc
Rom <romulo.santos@travelperk.com>
2022-07-18 13:09:24 +0200

    temporary, integrates apollo in a component

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,44 +1,26 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {ApolloClient, InMemoryCache, ApolloProvider, gql} from '@apollo/client';
+import {useQuery, gql} from '@apollo/client';
+import {PlayerData} from "./graphql/api-types";
 
-const client = new ApolloClient({
-    uri: 'http://localhost:3000/graphql',
-    cache: new InMemoryCache(),
-});
-
-client
-.query({
-    query: gql`
-        query GetPlayers {
-            players {
-                id
-                name
-            }
-        }
-    `,
-})
-.then((result) => console.log(result));
-
+const GET_PLAYERS = gql`
+    query GetPlayers {
+        players{id,name}
+    }
+`;
 
 function App() {
+    const {loading, error, data} = useQuery<PlayerData>(GET_PLAYERS);
     return (
         <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo"/>
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
+            {data && data.players.map(({id, name}) => (
+                <div>
+                    <pre>
+                    {name}
+                    </pre>
+                </div>
+            ))}
         </div>
     );
 }
---
+++ web/src/graphql/api-types.tsx
@@ -0,0 +1,8 @@
+export interface Player {
+    id: number;
+    name: string;
+}
+
+export interface PlayerData {
+    players: Player[];
+}
